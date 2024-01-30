76ba1dd3521139b97124b621d90d617fd47e69ae
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2024-01-30 14:14:21 +0100

    base.html added

--- htmx1/app1/views.py
+++ htmx1/app1/views.py
@@ -1,22 +1,8 @@
-# from django.http import HttpResponse
-
-# def index(request):
-#     return HttpResponse('hello django')
-
 
 from django.shortcuts import render
 
 def index(request):
     return render(request, "app1/template1.html", {"test": 'test1'})
-    
-def test(request):
-    return render(request, "app1/test.html", {"test": 'test1'})
-  
-# class test(request):
-#     template_name="app1/test.html"
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context["test"] = "test1"
-#         return context
-    
+def test(request):
+    return render(request, "app1/test.html", {"test_data": 'This is a test'})
