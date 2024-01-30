afe878f2c81addee543566aee7ae9828835818f0
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2024-01-30 14:26:13 +0100

    fix-base template extends

--- htmx1/htmx1/settings.py
+++ htmx1/htmx1/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'htmx1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "htmx1" /"templates",
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
