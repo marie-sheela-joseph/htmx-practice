86f024a351a4c764a7162869b06f97d2685f7ee9
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2024-01-30 22:49:05 +0100

    post request

--- htmx1/app1/urls.py
+++ htmx1/app1/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("test/", views.test, name="test"),
+    path("test_put/<int:id>/", views.test_put, name="test_put"),
 ]
--- htmx1/app1/views.py
+++ htmx1/app1/views.py
@@ -1,8 +1,13 @@
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, "app1/template1.html", {"test": 'test1'})
 
 def test(request):
     return render(request, "app1/test.html", {"test_data": 'This is a test'})
+
+def test_put(request, id):
+    print("request",request,"id",id)
+    return HttpResponse("hello post request")
